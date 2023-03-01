43b5a82b64f8860d34fe73dcf11ab5d879d60208
FernandoG <67986168+gomezfernando1995@users.noreply.github.com>
2023-03-01 19:04:59 -0300

    comit inicial
    proyecto finalizado

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const texto = document.querySelector(".texto");
+
+function verMas(){
+    if (texto.style.color!="blue"){
+        texto.style.color="blue";
+    }else{
+        texto.style.color="rgb(214, 212, 212)";
+    }
+    
+
+}
